c6ec59fef5413493e22c2f0c6ef12d5d05b4f847
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2023-06-14 22:44:28 +0530

    add-customers

--- src/components/AllCustomers.js
+++ src/components/AllCustomers.js
@@ -1,28 +1,48 @@
 
 import React from 'react'
-
-
-import '../styles/allcustomers.css'
+import '../styles/allcustomers.scss';
 
 const AllCustomers = () => {
-  
-  // getCustomers()
+
+  const personsData = [
+    { serialNo: 1, name: 'John Doe', accountNumber: '1234567890', balance: 5000 },
+    { serialNo: 2, name: 'Jane Smith', accountNumber: '0987654321', balance: 10000 },
+    { serialNo: 3, name: 'Michael Johnson', accountNumber: '5678901234', balance: 7500 },
+    { serialNo: 4, name: 'Michael Johnson', accountNumber: '5678901234', balance: 7500 },
+    { serialNo: 5, name: 'Michael Johnson', accountNumber: '5678901234', balance: 7500 },
+    { serialNo: 6, name: 'Michael Johnson', accountNumber: '5678901234', balance: 7500 },
+    // Add more data here...
+  ];
 
   return (
-    <>
-      <div></div>
+    <section className='customers_sec'>
 
-      <table className='customers_table'>
-        <tr className='headings'>
-          <th>Serial number</th>
-          <th>Name</th>
-          <th>Account number</th>
-          <th>Balance</th>
-        </tr>
-        
+      <div className='heading row justify_content_cntr'>
+        <h2>All Customers Information</h2>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Serial No</th>
+            <th>Name</th>
+            <th>Account Number</th>
+            <th>Current Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {personsData.map((person, index) => (
+            <tr key={index}>
+              <td>{person.serialNo}</td>
+              <td>{person.name}</td>
+              <td>{person.accountNumber}</td>
+              <td>{person.balance}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
 
-    </>
+    </section>
   )
 }
 
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react'
-import '../styles/allcustomers.css'
+import React from 'react';
+import '../styles/allcustomers.scss';
 
 const Transactions = () => {
+  // Generate random data for transactions
+  const generateRandomData = () => {
+    const transactionsData = [];
+    const statuses = ['Completed', 'Pending', 'Failed'];
 
+    for (let i = 1; i <= 10; i++) {
+      const transaction = {
+        serialNo: i,
+        debitedFrom: `Account ${Math.floor(Math.random() * 1000)}`,
+        creditedTo: `Account ${Math.floor(Math.random() * 1000)}`,
+        amount: Math.floor(Math.random() * 10000),
+        status: statuses[Math.floor(Math.random() * statuses.length)],
+      };
+      transactionsData.push(transaction);
+    }
+
+    return transactionsData;
+  };
+
+  const transactionsData = generateRandomData();
 
   return (
-    <>
-      <div></div>
+    <section className='transactions_cls'>
+      <div className="heading row justify_content_cntr">
+        <h2>All Transactions Information</h2>
+      </div>
 
-      <table className='customers_table'>
-        <tr className='headings'>
-          <th>Serial number</th>
-          <th>From</th>
-          <th>To</th>
-          <th>Amount transfered</th>
-          <th>Status</th>
-        </tr>
-        {/* {done.map((item, i) => {
-          return (
-            <tr key={i} className='values'>
-              <td>{i + 1}</td>
-              <td>{item.from}</td>
-              <td>{item.to}</td>
-              <td>{item.Amount_transfered}</td>
-              <td>{item.Status ? 'true' : 'false'}</td>
+      <table>
+        <thead>
+          <tr>
+            <th>Serial No</th>
+            <th>Debited From</th>
+            <th>Credited To</th>
+            <th>Amount</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactionsData.map((transaction, index) => (
+            <tr key={index}>
+              <td>{transaction.serialNo}</td>
+              <td>{transaction.debitedFrom}</td>
+              <td>{transaction.creditedTo}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.status}</td>
             </tr>
-          )
-        })} */}
+          ))}
+        </tbody>
       </table>
-    </>
-  )
-}
+    </section>
+  );
+};
 
 export default Transactions;
